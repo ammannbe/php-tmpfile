314f2b8dbb50489584888d8af7e59c16c3eddf75
benjamin <ba@softronics.ch>
2019-07-31 12:11:06 +0200

    split File::write() in write() and append()

--- src/TmpFile/File.php
+++ src/TmpFile/File.php
@@ -38,34 +38,70 @@ class File extends FS
     }
 
     /**
-     * Write/Append content to the file
+     * Write content to the file
+     * 
+     * This method overwrites existing content in the file
      *
+     * @see write_put_contents()
      * @param mixed $data Content to write
-     * @param int $flags = FILE_APPEND Flags for file_put_contents
      * @return int|false
      */
-    public function write($data, int $flags = FILE_APPEND)
+    public function write($data)
     {
         if ($this->exists()) {
-            return file_put_contents($this->path, $data, $flags);
+            return file_put_contents($this->path, $data);
         } else {
             return false;
         }
     }
 
     /**
-     * Write/Append array to the file
+     * Write array to the file
      *
      * Write each array value to a new line
      *
      * @see File::write();
+     * @see write_put_contents()
      * @param array $data Array to write
-     * @param int $flags = FILE_APPEND Flags for file_put_contents
      * @return int|false
      */
-    public function writeArray(array $data, int $flags = FILE_APPEND)
+    public function writeArray(array $data)
     {
-        // "\n" muss in doppelten Anführungszeichen stehen, ansonsten stimmt der Umbruch nicht
-        return $this->write(implode("\n", $data), $flags);
+        // "\n" must be in double quotation marks to create a correct line wrap
+        return $this->write(implode("\n", $data));
+    }
+
+    /**
+     * Append content to the file
+     * 
+     * This method will not overwrite any existing content in the file
+     *
+     * @see write_put_contents()
+     * @param mixed $data Content to write
+     * @return int|false
+     */
+    public function append($data)
+    {
+        if ($this->exists()) {
+            return file_put_contents($this->path, $data, FILE_APPEND);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Append array to the file
+     *
+     * Write each array value to a new line
+     *
+     * @see File::append();
+     * @see write_put_contents()
+     * @param array $data Array to write
+     * @return int|false
+     */
+    public function appendArray(array $data)
+    {
+        // "\n" must be in double quotation marks to create a correct line wrap
+        return $this->append(implode("\n", $data));
     }
 }
